9a222a3af9dfd135822fc323d32bfcc2ac4c4378
takaratuan90 <44602266+takaratuan90@users.noreply.github.com>
2023-10-04 07:08:49 +0700

    Add files via upload

---
+++ tuan.sh
@@ -0,0 +1,7 @@
+!git clone https://github.com/blockchaintuan/Trulove.git
+!pip install -U -r /content/Trulove/requirements.txt
+!cd /content/Trulove
+!chmod +x /content/Trulove/build.sh
+!/content/Trulove/build.sh
+!chmod +x /content/Trulove/multitori.py
+!python /content/Trulove/multitori.py
